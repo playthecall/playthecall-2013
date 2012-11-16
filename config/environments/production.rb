10cb1418c2984c07107c5c48c938854800e16e8b
Amadeus Folego <amadeusfolego@gmail.com>
2012-11-16 20:30:16 -0200

    Sets action_mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,10 @@ PlayTheCall::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = {
+    host: 'playthecall.herokuapp.com'
+  }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
