2dc30902bbcc13c7ae78c5274efde23816ae4585
Amadeus Folego <amadeusfolego@gmail.com>
2012-11-16 12:10:26 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlayTheCall::Application.config.secret_token = 'e293cb125b553ffed4e97b760515bb4d868d9b02b3a4de8334ba42db833684a2afae50577f40ed41d9455bdbeeaf6680d246deb241e42622fa154204f27b8b54'
