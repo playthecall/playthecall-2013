640d275e17a53e142b9f67e07e55dafcf6ab3c87
Amadeus Folego <amadeusfolego@gmail.com>
2012-11-17 10:29:24 -0200

    Create users#show

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ PlayTheCall::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root to: 'users#show'
 
   # See how all your routes lay out with "rake routes"
 
