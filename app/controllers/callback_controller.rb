007a291cce8a6e7364f8cdd1dac2585eb0f607d3
Dalton <dalthon@dlt.local>
2012-11-17 23:01:06 -0200

    Removed attempt of using Facebook Realtime API

--- app/controllers/callback_controller.rb
+++
@@ -1,9 +0,0 @@
-class CallbackController < ApplicationController
-  def fbrealtime
-    puts "="*80
-    puts YAML.dump(params)
-    puts "="*80
-    puts params.inspect
-    render text: params['hub.challenge']
-  end
-end
