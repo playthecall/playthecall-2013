6b267604d1717a6ab620409d20c67c852eb46283
Dalton <dalthon@dlt.local>
2012-11-19 20:05:20 -0200

    Ops... commited binding.pry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def choose_locale
-    binding.pry
     I18n.locale = Translation.for domain: request.host, user: current_user
-    binding.pry
   end
 end
