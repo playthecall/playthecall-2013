7ce474f551977aeff0e946513a7727540160bf62
Amadeus Folego <amadeusfolego@gmail.com>
2012-11-20 13:59:33 -0200

    Sets up compilation off assets

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module PlayTheCall
@@ -60,6 +60,10 @@ module PlayTheCall
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
+    config.assets.precompile += ['active_admin.css.scss', 'active_admin.js']
+
   end
 end
 
