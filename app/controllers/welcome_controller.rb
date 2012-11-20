8aad8c3bca70c163c7fa0832226f0445a28c26d6
Amadeus Folego <amadeusfolego@gmail.com>
2012-11-20 01:50:50 -0200

    Shows random users at the globe

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,11 @@ class WelcomeController < ApplicationController
 
   def index
     redirect_to user_path current_user if user_signed_in?
+
+    @cities = City.joins(:users)
+    @users = []
+    for city in @cities
+      @users << city.users.first(order: "RANDOM()")
+    end
   end
 end
