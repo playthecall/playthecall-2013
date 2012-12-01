2d45a8338787882b0e71b52b0bdc8eab5281cf4a
Amadeus Folego <amadeusfolego@gmail.com>
2012-12-01 20:58:18 -0200

    Unifies multiple layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,4 @@ module ApplicationHelper
   def st(*args, &block)
     t(*args, &block).html_safe
   end
-
-  def parent_layout(layout)
-    @view_flow.set(:layout,output_buffer)
-    self.output_buffer = render file: "layouts/#{layout}"
-  end 
 end
