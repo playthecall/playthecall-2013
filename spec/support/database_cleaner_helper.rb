225ef907a2d50ecf9c207eb9da7047598831f6bb
Amadeus Folego <amadeusfolego@gmail.com>
2012-12-01 19:21:25 -0200

    Fix databse_cleaner typo

--- spec/support/database_cleaner_helper.rb
+++ spec/support/database_cleaner_helper.rb
@@ -1,13 +1,13 @@
 module DatabaseCleanerHelper
   def clean_with_transaction_on(target)
-    databse_cleaner :transaction, target
+    database_cleaner :transaction, target
   end
 
   def clean_with_truncate_on(target)
-    databse_cleaner :truncate, target
+    database_cleaner :truncate, target
   end
 
-  def databse_cleaner(strategy, target)
+  def database_cleaner(strategy, target)
     before(target) do
       DatabaseCleaner.strategy = strategy
       DatabaseCleaner.start
