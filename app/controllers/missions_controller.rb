002990f971d53218dc4df6de6ac36cba865e13a4
Dalton <dalthon@dlt.local>
2012-12-01 17:09:29 -0200

    Missions details

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,6 +1,12 @@
 class MissionsController < ApplicationController
   before_filter :authenticate_user!
 
+  def index
+    @user    = current_user
+    @mission = @user.current_mission_enrollment.mission
+    render :show
+  end
+
   def show
     @user    = current_user
     @mission = Mission.find params[:id]
