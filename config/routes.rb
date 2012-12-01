5231e5941c80663ad5a089c577453a149a799616
Amadeus Folego <amadeusfolego@gmail.com>
2012-12-01 21:45:51 -0200

    Fix logout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 PlayTheCall::Application.routes.draw do
+
   devise_for :users,
-             controllers: { omniauth_callbacks: "users/omniauth_callbacks" } do
-    get "/users/sign_out" => "devise/sessions#destroy",
-        as: :destroy_user_session
-  end
+             controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   ActiveAdmin.routes(self)
 
