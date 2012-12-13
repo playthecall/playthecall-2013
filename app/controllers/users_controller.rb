3204ecbb2f4017b9c4958be9f51c34f2e3374fd0
Dalton <dalthon@dlt.local>
2012-12-12 22:38:12 -0200

    Cleaned up users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :check_users
 
   def edit
-    @user = logged_user params[:id]
   end
 
   def update
-    @user = logged_user params[:id]
     if @user.update_attributes params[:user]
       redirect_to missions_path
     else
@@ -16,12 +15,8 @@ class UsersController < ApplicationController
 
   private
 
-  def logged_user(id)
-    user = User.find id
-    if current_user == user
-      user
-    else
-      redirect_to :root
-    end
+  def check_users
+    @user = User.find params[:id]
+    redirect_to :root unless current_user == @user
   end
 end
