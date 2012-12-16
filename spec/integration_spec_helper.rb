ffe361d9a390c58f1ea1f860b73f1899298184c4
Amadeus Folego <amadeusfolego@gmail.com>
2012-12-16 10:10:06 -0200

    Adds TranslationHelper methods in rspec

--- spec/integration_spec_helper.rb
+++ spec/integration_spec_helper.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 Capybara.javascript_driver = :poltergeist
 
+RSpec.configure do |config|
+  config.include ActionView::Helpers::TranslationHelper
+end
+
 def login(user)
   visit new_user_session_path
-  fill_in I18n.t('activerecord.attributes.user.email'), with: user.email
-  fill_in I18n.t('activerecord.attributes.user.password'), with: user.password
-  click_button I18n.t('actions.sign_in')
+  fill_in t('activerecord.attributes.user.email'), with: user.email
+  fill_in t('activerecord.attributes.user.password'), with: user.password
+  click_button t('actions.sign_in')
 end
