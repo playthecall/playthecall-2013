2651fd679eba3e7482f60d9d18930afc4f58da2c
Amadeus Folego <amadeusfolego@gmail.com>
2012-12-16 10:03:58 -0200

    Uses translations instead of text

--- spec/integration_spec_helper.rb
+++ spec/integration_spec_helper.rb
@@ -4,7 +4,7 @@ Capybara.javascript_driver = :poltergeist
 
 def login(user)
   visit new_user_session_path
-  fill_in 'Email', with: user.email
-  fill_in 'Password', with: user.password
-  click_button 'Sign in'
+  fill_in I18n.t('activerecord.attributes.user.email'), with: user.email
+  fill_in I18n.t('activerecord.attributes.user.password'), with: user.password
+  click_button I18n.t('actions.sign_in')
 end
