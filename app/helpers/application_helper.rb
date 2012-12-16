d23bc355145b1b0e6a9312e1996fd4d76ccc5226
Enio Lopes <eniolopes@gmail.com>
2012-12-16 03:58:45 -0200

    Uses devise helper instead of depending on ivar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,6 @@ module ApplicationHelper
     t(*args, &block).html_safe
   end
 
-  def aside_present?
-    @user.present? and not @user.new_record?
-  end
-
   def image_url(path)
     "#{url_options[:host]}/#{image_path path}"
   end
