20af4f49b90a0825a7bb6762b65bf138b5193890
Fabiano Beselga <fabianobeselga@gmail.com>
2012-12-19 06:39:39 -0200

    fix bug while editing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
 
   before_filter :authenticate_user!
   before_filter :check_users
+  before_filter :load_user_country
+  before_filter :load_countries
+  before_filter :load_cities
 
   def edit
   end
@@ -20,5 +23,13 @@ class UsersController < ApplicationController
   def check_users
     @user = User.find params[:id]
     redirect_to :root unless current_user == @user
+  end
+
+  def load_user_country
+    @country = @user.city.country
+  end
+
+  def load_cities
+    @cities = City.where(:country_id => @country.id)
   end
 end
