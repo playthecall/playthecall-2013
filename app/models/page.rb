4e3acd3a238d0f4b589ad3a136e9a373f32489dd
Raul Souza Lima <raulsouzalima@gmail.com>
2012-12-19 07:03:00 -0200

    Added markdown to page content

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,12 @@
 class Page < ActiveRecord::Base
-  attr_accessible :content, :locale, :name
+  before_save :compile_content
+  attr_accessible :content, :locale, :name, :html_description
+
+  protected
+  def compile_content
+    if content_changed?
+      self.html_description = RDiscount.new(content).to_html
+    end
+
+  end
 end
