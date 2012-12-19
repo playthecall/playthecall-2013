79ca7689350866a5c96244d89cf89505b0a62e65
Raul Souza Lima <raulsouzalima@gmail.com>
2012-12-19 06:35:21 -0200

    Find page by id and render if it's the same current locale

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,9 @@
 class PagesController < InheritedResources::Base
   layout 'static'
+
+  def show
+    @page = Page.find(params[:id])
+    raise ActionController::RoutingError.new('Not Found') if @page.locale.to_sym != locale
+    super
+  end
 end
