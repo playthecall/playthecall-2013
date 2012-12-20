939031be20302b35da1b7cee1ab80b61763c0a24
Philip Sampaio <philip.sampaio@gmail.com>
2012-12-20 13:18:54 -0200

    Using content_tag instead of html in embedded_video helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,7 +43,7 @@ module ApplicationHelper
   def link_to_remove_fields(name, f)
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
-  
+
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
@@ -53,7 +53,7 @@ module ApplicationHelper
   end
 
   def embedded_video(url)
-    "<iframe width='640' height='360' src='#{url}' frameborder='0' allowfullscreen></iframe>".html_safe
+    content_tag(:iframe,"",src: url, frameborder: '0', allowfullscreen: 'allowfullscreen', width: 640,height: 360)
   end
 
 end
