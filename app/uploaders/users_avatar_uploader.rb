01a065b4c5384e020cec1641b8c3d63aff50700c
Thiago Silva <thiago.kiler6@gmail.com>
2012-12-21 10:36:43 -0200

    Change avatar upload directory

--- app/uploaders/users_avatar_uploader.rb
+++ app/uploaders/users_avatar_uploader.rb
@@ -3,4 +3,8 @@ class UsersAvatarUploader < CarrierWave::Uploader::Base
   setup_storage
 
   process resize_to_fill: [180, 180]
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
 end
