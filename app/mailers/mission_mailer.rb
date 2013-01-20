73e3f25d495dc917172450c3c0717ed4890b7c5b
Amadeus Folego <amadeusfolego@gmail.com>
2013-01-20 03:18:44 -0200

    Sets the default from for mailer

--- app/mailers/mission_mailer.rb
+++ app/mailers/mission_mailer.rb
@@ -1,4 +1,6 @@
 class MissionMailer < ActionMailer::Base
+  default from: 'no-reply@playthecall.com'
+
   def accomplished_mission(mission_enrollment, user)
     @mission = mission_enrollment.mission
     mail(:to => user.email, :subject => I18n.t('mails.accomplished_mission.subject'))
