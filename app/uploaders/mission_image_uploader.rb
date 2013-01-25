5e3ee0cd4d844339a28d82d3f69af32bfdfda5a2
Samuel Barroso <samuel.barroso@codeminer42.com>
2013-01-25 00:02:58 -0300

    fixes image upload size

--- app/uploaders/mission_image_uploader.rb
+++ app/uploaders/mission_image_uploader.rb
@@ -3,6 +3,6 @@ class MissionImageUploader < CarrierWave::Uploader::Base
   setup_storage
 
   version :medium do
-    process resize_to_fit: [680, nil]
+    process resize_to_fit: [680, 354]
   end
 end
