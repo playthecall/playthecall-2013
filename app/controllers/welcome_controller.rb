c6eb220ac90b04e87f250916861037103adc60c4
Philip Sampaio <philip.sampaio@gmail.com>
2013-01-30 01:07:11 -0200

    The end of chapter message When user archive all missions in the current chapter For now we don't have the mechanism to upgrade the user's chapter. The pt-BR locale should be updated. Related to #159

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,24 @@
 class WelcomeController < ApplicationController
   layout 'welcome'
-
-  before_filter lambda { redirect_to user_current_mission },
-                if: :user_signed_in?
+  before_filter :mission_redirect
 
   def index
     @cities = City.joins(:users)
     @users = []
     for city in @cities
       @users << city.users.first(order: "RANDOM()")
+    end
+  end
+
+  private
+
+  def mission_redirect
+    if user_signed_in?
+      if current_user.current_mission.present?
+        redirect_to user_current_mission
+      elsif Chapter.finished?(current_user.current_chapter, current_user)
+        redirect_to end_of_chapter_path
+      end
     end
   end
 end
