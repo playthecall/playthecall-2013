3820759634ee20824fb3bfd79e933ccb8ec16153
Philip Sampaio <philip.sampaio@gmail.com>
2013-09-19 03:26:06 -0300

    configuring secret token to use env variable in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PlayTheCall::Application.config.secret_token = 'e293cb125b553ffed4e97b760515bb4d868d9b02b3a4de8334ba42db833684a2afae50577f40ed41d9455bdbeeaf6680d246deb241e42622fa154204f27b8b54'
+PlayTheCall::Application.config.tap do |config|
+  if Rails.env.production?
+    config.secret_token = ENV['PLAYTHECALL_SECRET']
+  else
+    config.secret_token = 'e293cb125b553ffed4e97b760515bb4d868d9b02b3a4de8334ba42db833684a2afae50577f40ed41d9455bdbeeaf6680d246deb241e42622fa154204f27b8b54'
+  end
+end
